1276793d95a69c5f9bdae7f5525fe1ac4c5b2fde
mattoattacko <matt.mcquain@gmail.com>
2023-09-15 16:32:10 -0700

    data changes

--- src/data.js
+++ src/data.js
@@ -5,18 +5,30 @@ export const foodData = {
     { name: "Dagashi-Bar", note: "Bar with sweets that Justin found", time: "5 mins" },
     { name: "つくね・唐揚げ・レモンサワー 手ごね屋 渋谷店", note: "Tsukune and all you can drink sours from Tiktok", time: "5 mins" },
     { name: "Purin-ya san", note: "Pudding shop kris found on TikTok", time: "10 mins" },
-    { name: "Iyoshi Cola", note: "Saw on TikTok. Great craft cola", time: "10 mins" }
+    { name: "Iyoshi Cola", note: "Saw on TikTok. Great craft cola", time: "10 mins" },
+    { name: "Mini Pig Cafe", note: "Mini pig cafe in Shibuya", time: "10 mins" },
+    { name: "HARRY HARAJUKU terrace", note: "Might be otters here", time: "10 mins" },
   ],
   "Shinjuku Area": [
     { name: "Omoide Yokochō", note: "Very aesthetic. Many small food places. TikTok", time: "5 mins" },
     { name: "Ark Lounge 新宿西口店", note: "Italian food. TikTok", time: "5 mins" },
-    { name: "新宿カブキhall~歌舞伎横丁", note: "Bunch of crazy looking little izakaya shops to eat at. TikTok", time: "5 mins" }
+    { name: "新宿カブキhall~歌舞伎横丁", note: "Bunch of crazy looking little izakaya shops to eat at. TikTok", time: "5 mins" },
+    { name: "BOX cafe&space ルミネエスト新宿店", note: "Possible Mofusand collab cafe. Emirichu", time: "5 mins" },
+    { name: "BAR GOLD FINGER", note: "Was on Queer Eye TV", time: "5 mins" },
+  ],
+  "Ikebukuro Area": [
+    { name: "Animate Cafe Ikebukuro", note: "Maybe cool collab cafes. Emirichu.", time: "5 mins" },
+    { name: "KitKat Chocolatory", note: "Make KitKats", time: "5 mins" },
   ],
   "Ueno Area": [
     { name: "Nikuya no Daidokoro Ueno", note: "Benji and JB went here. Food looks amazing", time: "5 mins" }
   ],
   "Central Tokyo": [
-    { name: "A Happy Pancake Ginza", note: "Good pancakes", time: "5 mins to Ginza Station" }
+    { name: "A Happy Pancake Ginza", note: "Good pancakes", time: "5 mins to Ginza Station" },
+    { name: "Tokyo Banana World", note: "Tokyo Banana Shop in Tokyo Station", time: "5 mins to Ginza Station" },
+    { name: "DAWN | Avatar Robot Cafe ver.β", note: "Robot Cafe", time: "5 mins to Nihonbashi Station" },
+    { name: "Pokémon Café Near Nihonbashi", note: "Need reservations", time: "5 mins to Nihonbashi Station" },
+    { name: "Sanrio Garden Cafe", note: "Brand new Sanrio collab cafe! Emirichu", time: "5 mins to Ueno Station" },
   ],
   "Yokohama Area": [
     { name: "Togoshiya", note: "Dive into Japan TikTok said so good", time: "5 mins to Gotanda Station" },
@@ -35,7 +47,8 @@ export const shoppingData = {
     { name: "Kiddy Land", note: "Toy shop", time: "5 mins" },
     { name: "2nd STREET Harajuku", note: "Shopping area", time: "10 mins" },
     { name: "Kinji Used Clothing Harajuku Branch", note: "Use clothing store in Harajuku", time: "10 mins" },
-    { name: "YM Square Harajuku", note: "Shopping area in Harajuku", time: "10 mins" }
+    { name: "YM Square Harajuku", note: "Shopping area in Harajuku", time: "10 mins" },
+    { name: "Ikspiari", note: "Really nice outdoor shopping area with a Disney store and Outback Steakhouse", time: "15 mins" },
   ],
   "Shinjuku Area": [
     { name: "namco TOKYO", note: "The games above the izakaya area. TikTok", time: "5 mins" }
@@ -53,10 +66,18 @@ export const shoppingData = {
   ],
   "Asakusa Area": [
     { name: "Nakamise-dori Street", note: "Shopping street from TikTok", time: "5 mins" }
+  ],  
+  "Ueno Area": [
+    { name: "Ameyoko Shopping District", note: "Outdoor shopping street on tiktok", time: "5 mins" },
+    { name: "Hard Off & Hobby Off", note: "cool used stuff", time: "10 mins" },
   ],
   "Central Tokyo": [
     { name: "Dover Street Market Ginza", note: "Fancy street wear", time: "5 mins to Ginza Station" },
-    { name: "Tokyo Banana World (Tokyo Gift Palette)", note: "Tokyo Banana shop", time: "2 mins to Tokyo Station" }
+    { name: "Tokyo Banana World (Tokyo Gift Palette)", note: "Tokyo Banana shop", time: "2 mins to Tokyo Station" },
+    { name: "Nihonbashi", note: "Shopping Area", time: "@Nihonbashi Station" },
+    { name: "谷中トート(totetoart)", note: "Super cute totebags", time: "5 mins to Nippori Station" },
+    { name: "Nakano Broadway", note: "Shopping Arcade (some underground)", time: "5 mins to Nakano Station" },
+    { name: "BOOKOFF SUPER BAZAAR SEIYU", note: "Omori Store Book Off", time: "5 mins to Omori Station" },
   ],
   "Yokohama Area": [
     { name: "Nissan Heritage Collection", note: "Nissan Heaven", time: "15 mins" },
@@ -71,13 +92,7 @@ export const shoppingData = {
 export const sightseeingData = {
   "Shibuya/Harajuku Area": [
     { name: "Shibuya Sky", note: "Beautiful view from Tiktok", time: "5 mins" },
-    { name: "HARRY HARAJUKU terrace", note: "Apparently there are otters here", time: "10 mins" },
-    { name: "mipig cafe - Harajuku", note: "mini pig cafe in Shibuya", time: "10 mins" },
     { name: "Ikspiari", note: "Really nice outdoor shopping area with a Disney store and Outback Steakhouse", time: "15 mins" }
-  ],
-  "Shinjuku Area": [
-    { name: "BOX cafe&space ルミネエスト新宿店", note: "Possible Mofusand collab cafe. Emirichu", time: "5 mins" },
-    { name: "BAR GOLD FINGER", note: "Was on Queer Eye", time: "5 mins" }
   ],
   "Ikebukuro Area": [
     { name: "Nishi-Ikebukuro Park", note: "IWGP", time: "5 mins" }
@@ -92,14 +107,13 @@ export const sightseeingData = {
   ],
   "Ueno Area": [
     { name: "Ameyoko Shopping District", note: "Outdoor shopping street on tiktok", time: "5 mins" },
-    { name: "Hard Off & Hobby Off", note: "Cool used stuff", time: "10 mins" }
   ],
   "Central Tokyo": [
     { name: "Roppongi Hills Mori Tower", note: "Tower to get a great view", time: "10 mins to Roppongi Station" },
     { name: "Jingu Gaien Ginkgo Avenue", note: "Super beautiful Ginkgo trees", time: "5 mins to Aoyama-Itchome Station" },
     { name: "BINGO SPORTS 東京ショールーム", note: "cool car dealer I saw on Tiktok.", time: "10 mins to Kamiyacho Station" },
     { name: "Maison Hermès", note: "Cool architecture Hermes shop", time: "5 mins to Ginza Station" },
-    { name: "DAWN | Avatar Robot Cafe ver.β", note: "Robot Cafe!", time: "5 mins to Nihonbashi Station" },
+
     { name: "Pokémon Café", note: "Near Nihonbashi. Need to make way advanced reservations", time: "5 mins to Nihonbashi Station" },
     { name: "Nihonbashi", note: "Shopping area. Nihonbashi Station", time: "5 mins to Nihonbashi Station" },
     { name: "Gokokuji Temple", note: "Temple with a lot of cat figures from tiktok", time: "10 mins to Edogawabashi Station" },
@@ -109,10 +123,8 @@ export const sightseeingData = {
     { name: "Yomiuri Land", note: "Theme park", time: "5 mins to Keio Yomiuri Land Station" },
     { name: "Hotel UFO", note: "Cool Love Hotel", time: "15 mins to Shin-Kiba Station" },
     { name: "Nakano Broadway", note: "Shopping Arcade (some underground)", time: "5 mins to Nakano Station" },
-    { name: "Sanrio characters garden cafe", note: "Brand new Sanrio collab cafe! Emirichu", time: "5 mins to Ueno Station" },
     { name: "Yamashita Station", note: "This is the station we go to if we want to go to the cat temple. Yamashita Station", time: "5 mins to Yamashita Station" },
     { name: "ART AQUARIUM MUSEUM", note: "Amazing fish art stuff on tiktok", time: "5 mins to Ginza Station" },
-    { name: "BOOKOFF SUPER BAZAAR SEIYU Omori Store", note: "Book off!", time: "5 mins to Omori Station" },
     { name: "TOKYO MEGA ILLUMINATION", note: "Beautiful lights. Opens Oct 15th Sat", time: "10 mins to Shinagawa Station" }
   ],
   "Tokyo Disney Area": [
