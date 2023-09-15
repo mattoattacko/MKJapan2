d391f1cb653335fded96fd8c5b33256b4506a43a
mattoattacko <matt.mcquain@gmail.com>
2023-09-15 16:04:48 -0700

    first push of completed files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,123 @@
-import logo from './logo.svg';
+// App.js
+
+import React, { useState } from 'react';
+
 import './App.css';
 
+import { foodData, shoppingData, sightseeingData } from './data';
+
 function App() {
+  const [activeTab, setActiveTab] = useState('food');
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>M&K Japan Exploration</h1>
       </header>
+
+      <nav>
+        <button
+          className={activeTab === 'food' ? 'tab active' : 'tab'}
+          onClick={() => setActiveTab('food')}  
+        >
+          Food & Drink
+        </button>
+
+        <button
+          className={activeTab === 'shopping' ? 'tab active' : 'tab'}
+          onClick={() => setActiveTab('shopping')}
+        >
+          Shopping
+        </button>
+
+        <button
+          className={activeTab === 'sightseeing' ? 'tab active' : 'tab'}
+          onClick={() => setActiveTab('sightseeing')}  
+        >
+          Sightseeing
+        </button>
+      </nav>
+
+      <main>
+        {activeTab === 'food' && <FoodList data={foodData} />}
+        {activeTab === 'shopping' && <ShoppingList data={shoppingData} />}
+        {activeTab === 'sightseeing' && <SightseeingList data={sightseeingData} />}
+      </main>
+    </div>
+  );
+}
+
+function FoodList({ data }) {
+  return (
+    <div className="content">
+      {Object.keys(data).map(region => (
+        <div key={region}>
+          <h2>{region}</h2>
+          
+          <table>
+            <tbody>
+              {data[region].map(item => (
+                <tr key={item.name}>
+                  <td>{item.name}</td>
+                  <td>{item.note}</td>
+                  <td>{item.time}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function ShoppingList({data}) {
+  return (
+    <div className="content">
+      {Object.keys(data).map(region => (
+        <div key={region}>
+          <h2>{region}</h2>
+          
+          <table>
+            <tbody>
+              {data[region].map(item => (
+                <tr key={item.name}>
+                  <td>{item.name}</td>
+                  <td>{item.note}</td>
+                  <td>{item.time}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+          
+        </div>
+      ))}
+    </div>
+  );
+}
+
+function SightseeingList({data}) {
+  return (
+    <div className="content">
+       {Object.keys(data).map(region => (
+         <div key={region}>
+           <h2>{region}</h2>
+           
+           <table>
+             <tbody>
+               {data[region].map(item => (
+                 <tr key={item.name}>
+                   <td>{item.name}</td>
+                   <td>{item.note}</td>
+                   <td>{item.time}</td>
+                 </tr>
+               ))}
+             </tbody>
+           </table>
+           
+         </div>
+       ))}
     </div>
   );
 }
